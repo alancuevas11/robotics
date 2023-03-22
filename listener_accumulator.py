ff648a265cfe6715468ca8a715c8130ea81effbf
alancuevas11 <127822649+alancuevas11@users.noreply.github.com>
2023-03-21 23:43:46 -0400

    Update listener_accumulator.py

--- listener_accumulator.py
+++ listener_accumulator.py
@@ -10,7 +10,8 @@ message_buffer = []
 start_time = None
 
 def callback(msg):
-    """ Callback to receive a message and concatenate it with other messages received within a span of 3 seconds """
+    """ Callback to receive a message and concatenate it with 
+    other messages received within a span of 3 seconds """
     global pub, repeater, message_buffer, start_time
 
     # Check if this is the first message
@@ -33,29 +34,23 @@ def callback(msg):
 
         # Reset the buffer and start time for the next set of messages
         message_buffer = []
-        start_time = None
+        START_TIME = None
 
         # Show some logging information (optional)
         repeater.get_logger().info('Concatenated messages: ' + concatenated_message)
 
 def main(args=None):
     """Node setup and main ROS loop"""
-    global pub, repeater
-
     # Init node. anonymous=True allows multiple launch with automatically assigned names
     rclpy.init(args=args)
     repeater = rclpy.create_node('repeater')
-
     # Prepare subscriber on a topic, and publisher on another
     repeater.create_subscription(String, 'chatter', callback, 10)
     pub = repeater.create_publisher(String, 'chatter_repeated', 10)
-
     rclpy.spin(repeater)
-
     # node cleanup
     repeater.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     try:
