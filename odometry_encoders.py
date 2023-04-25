19fef2d7a9aba7349bbca9f2600050b60a9b2ff5
alancuevas11 <127822649+alancuevas11@users.noreply.github.com>
2023-04-24 22:01:37 -0400

    Update odometry_encoders.py

--- odometry_encoders.py
+++ odometry_encoders.py
@@ -2,10 +2,6 @@
 """
 Publishes odometry using Euler integration from motor speeds.
 """
-
-from math import pi
-
-import me416_utilities as mu
 import numpy as np
 import rclpy
 import robot_model as rm
@@ -25,8 +21,10 @@ class Odometry(Node):
         self.subscriber= self.create_subscription(MotorSpeedsStamped, 'encoders',
                                                   self.encoders_callback, 10)
         self.timer = self.create_timer(0.5, self.timer_callback)
-        self.z_state= 0  # Initialize state estimate to zero vector
-        self.register = rm.StampedMsgRegister()  # Create object to keep track of time
+        # Initialize state estimate to zero vector
+        self.z_state= 0
+        # Create object to keep track of time
+        self.register = rm.StampedMsgRegister()
 
     def timer_callback(self):
         '''
@@ -34,31 +32,43 @@ class Odometry(Node):
         '''
         msg = PoseStamped()
         # Fill orientation
-        theta = self.z_state  # Extract orientation from state estimate
-        quat = euler2quat(0, 0, theta)  # Convert Euler angles to quaternion
-        msg.pose.orientation.x = quat[1]  # Fill quaternion components
+        # Extract orientation from state estimate
+        theta = self.z_state
+        # Convert Euler angles to quaternion
+        quat = euler2quat(0, 0, theta)
+        # Fill quaternion components
+        msg.pose.orientation.x = quat[1]
         msg.pose.orientation.y = quat[2]
         msg.pose.orientation.z = quat[3]
-        msg.pose.orientation.w = quat[0] 
-        msg.pose.position.x = self.z_state[0]  # Fill position components
+        msg.pose.orientation.w = quat[0]
+        # Fill position components
+        msg.pose.position.x = self.z_state[0]
         msg.pose.position.y = self.z_state[1]
-        msg.pose.position.z = 0  # Set z position to zero (2D planar case, no change in height)
-        msg.header.stamp = self.get_clock().now().to_msg()  # Get current time and set message header
-        msg.header.frame_id = 'map'  # Set frame ID
-        self.publisher.publish(msg)  # Publish message
-        
+        # Set z position to zero (2D planar case, no change in height)
+        msg.pose.position.z = 0
+        # Get current time and set message header
+        msg.header.stamp = self.get_clock().now().to_msg()
+         # Set frame ID
+        msg.header.frame_id = 'map'
+        # Publish message
+        self.publisher.publish(msg)
     def encoders_callback(self, msg):
         '''
         Integrate the motor speeds in pose estimate using Euler's method
         '''
-        stepsize = rm.StampedMsgRegister.replace_and_compute_delay(self,msg)  # Compute time step between messages
-        speed_linear = msg.twist.linear.x  # Extract linear speed from message
-        speed_angular = msg.twist.angular.z  # Extract angular speed from message
+        # Compute time step between messages
+        stepsize = rm.StampedMsgRegister.replace_and_compute_delay(self,msg)
+        # Extract linear speed from message
+        speed_linear = msg.twist.linear.x
+        # Extract angular speed from message
+        speed_angular = msg.twist.angular.z
         # Convert linear and angular speeds to motor speeds
         u_lw, u_rw = rm.twist_to_speeds(speed_linear, speed_angular)
         u_current = np.array([u_lw, u_rw])
-        z_next = rm.euler_step(self.z_state, u_current, stepsize)  # Integrate state estimate using Euler's method
-        self.z_state= z_next  # Update state estimate
+        # Integrate state estimate using Euler's method
+        z_next = rm.euler_step(self.z_state, u_current, stepsize)
+        # Update state estimate
+        self.z_state= z_next
 
 def main(args=None):
     '''
